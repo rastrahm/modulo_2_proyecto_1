e0c55e45dfdd5d2cf467738420e92de4524703e3
root <rastrahm@gmail.com>
2025-09-29 09:16:35 -0300

    Prueba

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,21 +7,16 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <
-    React.StrictMode >
-    <
-    BrowserRouter >
-    <
-    App / >
-    <
-    ToastContainer position = "top-right"
-    autoClose = { 5000 }
-    hideProgressBar = { false }
-    newestOnTop = { false }
-    closeOnClick rtl = { false }
-    pauseOnFocusLoss draggable pauseOnHover theme = "light" /
-    >
-    <
-    /BrowserRouter> <
-    /React.StrictMode>
+root.render( 
+    <React.StrictMode >
+        <BrowserRouter>
+            <App />
+            <ToastContainer position = "top-right"
+                autoClose = { 5000 }
+                hideProgressBar = { false }
+                newestOnTop = { false }
+                closeOnClick rtl = { false }
+                pauseOnFocusLoss draggable pauseOnHover theme = "light"/>
+        </BrowserRouter> 
+    </React.StrictMode>
 );
